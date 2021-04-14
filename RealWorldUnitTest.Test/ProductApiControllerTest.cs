c4babcb0d86249a26d589b24b255be03913d0661
MeralTd <meraltasdemir77@gmail.com>
2021-04-14 14:14:39 +0300

    Test the GetProducts() and GetProduct(id) method

--- RealWorldUnitTest.Test/ProductApiControllerTest.cs
+++ RealWorldUnitTest.Test/ProductApiControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using RealWorldUnitTest.Web.Controllers;
 using RealWorldUnitTest.Web.Models;
 using RealWorldUnitTest.Web.Repository;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace RealWorldUnitTest.Test
 {
@@ -29,6 +31,48 @@ namespace RealWorldUnitTest.Test
             };
         }
 
-        
+        [Fact]
+        public async void GetProducts_ActionExecutes_ReturnOkResultWithProduct()
+        {
+            _mockRepo.Setup(x => x.GetAll()).ReturnsAsync(products);
+
+            var result = await _controller.GetProducts();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            var returnProducts = Assert.IsAssignableFrom<IEnumerable<Product>>(okResult.Value);
+
+            Assert.Equal<int>(2, returnProducts.ToList().Count);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public async void GetProduct_IdInValid_ReturnNotFound(int productId)
+        {
+            Product product = null;
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+            var result = await _controller.GetProduct(productId);
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async void GetProduct_IdValid_ReturnOkResult(int productId)
+        {
+            var product = products.First(x=>x.Id==productId);
+
+            _mockRepo.Setup(x => x.GetById(productId)).ReturnsAsync(product);
+
+            var result = await _controller.GetProduct(productId);
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnProduct = Assert.IsType<Product>(okResult.Value);
+
+            Assert.Equal(productId, returnProduct.Id);
+            Assert.Equal(product.Name, returnProduct.Name);
+        }
+
     }
 }
